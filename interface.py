a68f9f638795e6652e5a19a3935df5775e85ffef
Nadzeya <boyevanadzeya@gmail.com>
2023-12-10 15:58:03 +0300

    Add dataset CSV
    Instead of writing photos to dataset
    directory in JPG format add writing
    photos (NumPy arrays) to dataset CSV file.
    No dataset folder needed.

--- interface.py
+++ interface.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from PIL import Image
 import tkinter as tk
+import pandas as pd
 
 
 root = tk.Tk()
@@ -22,6 +23,10 @@ root.geometry('400x500+550+130')
 person_id = 0
 names = []
 name = ''
+dataset = pd.read_csv('dataframe.csv')
+people_df = dataset.iloc[:,0:2]
+people = pd.Series(people_df.name.values,index=people_df.id).to_dict()
+
 
 
 def introduce():
@@ -50,11 +55,10 @@ def introduce():
 
         ############### Adding new person to the dataset ###############
 
-        global person_id
+        global person_id, dataset, people
 
         #face_id = person_id
 	
-
         cam = cv2.VideoCapture(0)
         cam.set(3, 640) # set video width
         cam.set(4, 480) # set video height
@@ -67,7 +71,9 @@ def introduce():
         count = 0
         
         while(True):
-        
+            
+            global dataset
+
             # get faces from the camera
             ret, img = cam.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -76,7 +82,22 @@ def introduce():
             for (x,y,w,h) in faces:
                 cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
                 count += 1
-                img_numpy = np.array(cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2),'uint8')
+                img_numpy = np.array(cv2.rectangle(img, (x,y), (x+w,y+h), (255, 0, 0), 2), 'uint8')
+
+
+
+
+                # update dataset variable
+                dataset = pd.concat([
+                    dataset,
+                    pd.DataFrame([[person_id, name, img_numpy]], columns=['id', 'name', 'image'])
+                ],
+                axis = 0)
+                people = pd.Series(people_df.name.values,index=people_df.id).to_dict()
+                dataset.to_csv('dataframe.csv', index=False)
+
+
+
 
                 # Save photo to the datasets folder
                 #cv2.imwrite("dataset/User." + str(face_id) + '.' +
@@ -99,39 +120,38 @@ def introduce():
 
         print ("\n [INFO] I'm trying to memorize you. Wait ...")
 
-        dataset = pd.read_csv('dataset.csv')
-
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        recognizer = cv2.face.LBPHFaceRecognizer.create() 
 
         # function to get the images and label data
-        def getImagesAndLabels(path):
+        #def getImagesAndLabels(path):
 
             #imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
             
-            images_list = dataset('image').tolist()
-            names_list = dataset('name').tolist()
+            #images_list = dataset('image').tolist()
+            #names_list = dataset('name').tolist()
             
             #for name,image in zip(names_list, images_list):
 
-            #for imagePath in imagePaths:
-                #PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
-                #img_numpy = np.array(PIL_img,'uint8')
-                #id = int(os.path.split(imagePath)[-1].split(".")[1])
+            # for imagePath in imagePaths:
+            #     PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+            #     img_numpy = np.array(PIL_img,'uint8')
+            #     id = int(os.path.split(imagePath)[-1].split(".")[1])
                 
-                ##faces = face_detector.detectMultiScale(image)
+                #faces = face_detector.detectMultiScale(image)
 
-                ##for (x,y,w,h) in faces:
-                    ##faceSamples.append(image[y:y+h,x:x+w])
-                    ##names_list.append(name)
+                #for (x,y,w,h) in faces:
+                    #faceSamples.append(image[y:y+h,x:x+w])
+                    #names_list.append(name)
 	
-            ##return faceSamples,ids
+            #return faceSamples,ids
             
-
-        images_list = dataset('image').tolist()
-        names_list = dataset('name').tolist()
+        
+        images_list = dataset['image'].tolist()
+        id_list = dataset['id'].tolist()
+        #names_list = dataset['name'].tolist()
             
         #faces,ids = getImagesAndLabels(path)
-        recognizer.train(faces, np.array(ids))
+        recognizer.train(images_list, id_list)
 
         # Save the model into trainer/trainer.yml
         recognizer.save('trainer.yml') # recognizer.save() worked on Mac, but not on Pi
@@ -145,10 +165,10 @@ def introduce():
     intro.geometry('300x200+600+220')
     
     label_name = tk.Label(intro, text = 'Name')
-    label_name.place(x=60,y=55)
+    label_name.place(x = 60, y = 55)
     
     the_name = tk.Entry(intro)
-    the_name.place(x=120,y=55)
+    the_name.place(x = 120, y = 55)
 
     #the_name = input('\n Your name pleassss ==>  ')
     
@@ -158,8 +178,8 @@ def introduce():
     
     btn_take_photo = tk.Button(intro, text = 'Take photos',
               command = new_person)
-    btn_save.place(x=48,y=110)
-    btn_take_photo.place(x=170,y=110)
+    btn_save.place(x = 48,y = 110)
+    btn_take_photo.place(x = 170,y = 110)
 
     
 
@@ -167,16 +187,17 @@ btn_intro = tk.Button(frame, text = "I'm a new user",
                       command = introduce,
                       padx = 10, 
                       font = ('Arial', 14))
-btn_intro.place(x=120, y=110)
+btn_intro.place(x = 120, y = 110)
 
 
 
 def recognition():
 
-    global names, name
-    names.append(name)
+    global people 
+    #global names, name
+    #names.append(name)
 
-    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer = cv2.face.LBPHFaceRecognizer.create() 
     recognizer.read('trainer.yml')
 
     cascadePath = "haarcascade_frontalface_default.xml"
@@ -210,26 +231,43 @@ def recognition():
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-            # If confidence is less them 100 ==> "0" : perfect match
-            if (confidence < 100):
-                id = names[id]
-                print(names)
-                confidence = "  {0}%".format(round(100 - confidence))
+
+            print(people) ### empty dictionary ###################################################
+
+            # infer name from id
+            person = people[id]
+
+            certainty_threshold = 50
+
+            if (confidence > certainty_threshold):
+                printed_id = person
+                #print(names)
+                confidence = "  {0}%".format(round(confidence))
             else:
-                id = "unknown"
-                confidence = "  {0}%".format(round(100 - confidence))
+                printed_id = "unknown"
+                confidence = "  {0}%".format(round(confidence))
             
 
             # Write the name of recognized person
             cv2.putText(
                 img,
-                str(id),
+                str(printed_id),
                 (x + 5, y - 5),
                 font,
                 1,
                 (255, 255, 255),
                 2
             )
+            # Write how confident model is
+            cv2.putText(
+                img, 
+                str(confidence), 
+                (x + 5,y + h - 5), 
+                font, 
+                1, 
+                (255, 255, 0), 
+                1
+            ) 
               
             
         cv2.imshow('camera', img)
